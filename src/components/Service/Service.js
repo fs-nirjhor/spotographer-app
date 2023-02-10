07d54cf22ae7d4b4adbfbdeb124e810e88be8f1d
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-02-10 22:48:10 +0600

    commit

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -5,16 +5,16 @@ const Service = (props) => {
   const {image,title,price,details} = props.service;
 return (
   <Col>
-     <Card>
+     <Card className="h-100">
         <Card.Img variant="top" src={image} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Title>Price: ${price}</Card.Title>
           <Card.Text>{details}</Card.Text>
-          <Button variant="outline" className="my-btn w-50">
+        </Card.Body>
+          <Button variant="outline" className="my-btn w-50 m-3">
               BOOK NOW
             </Button>
-        </Card.Body>
       </Card>
   </Col>
 );
