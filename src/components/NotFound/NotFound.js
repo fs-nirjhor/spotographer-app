70c55ab07de9fc7799d6e6fb8fe5a3d9393025a4
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-02-18 23:17:50 +0600

    commit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,13 +1,13 @@
-import React from 'react';
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const NotFound = () => {
   return (
-    <div className="m-auto h-100 text-center ">
-      <h1 className="display-1 fw-bold font-monospace">4 0 4</h1>
-      <p className="display-6">Oops! The page you requested could not be found.</p>
-      <Link to="/" className="fw-bold text-decoration-none">Back to home</Link>
-    </div>
+    <Container className="m-auto h-100 text-center lh-base font-monospace">
+      <h1 className="display-1 fw-bold ">4 0 4</h1>
+      <p className="fs-3">Oops! The page you requested could not be found.</p>
+      <Link to="/" className="fw-bolder fs-5 text-decoration-none">Back to home</Link>
+    </Container>
   );
 };
 
